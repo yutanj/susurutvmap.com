ded1ca8c44bfecfc38ad65e01edf116f219196f6
yutanj <77373272+yutanj@users.noreply.github.com>
2021-03-02 21:12:29 +0900

    Update destination.php

--- path/to/destination.php
+++ path/to/destination.php
@@ -1,4 +1,16 @@
-<?php echo $_POST["total1"]; ?>
-<?php echo $_POST["total2"]; ?>
-<?php echo $_POST["username"] ?>
-<?php echo $_POST["contents"] ?>
+<?php
+require_once('dbc.php');
+error_reporting(E_ALL & ~E_NOTICE);
+$dbc = new Dbc;
+$user_input = $_POST;
+$dbh = $dbc->dbConnect();
+//var_dump($dbc->dbConnect());
+$dbc->blogCreate($user_input);
+//var_dump($dbc->blogCreate($blogs));
+$request = $dbc->getAllRequest();
+
+echo $request;
+
+?>
+<!--<a href="posted_confirmation.php">投稿を確認する→</a>-->
+<a href="mark_to_map2.php">地図で確認する</a>
